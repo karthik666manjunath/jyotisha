c900d5729672cdb5e2a4dd128dc10670bd4df977
vvasuki <vishvas.vasuki@gmail.com>
2020-10-08 11:35:49 +0530

    Better tests. test_month fails. anamolous 0 valued sunrise tithi

--- tests/spatio_temporal/test_daily.py
+++ tests/spatio_temporal/test_daily.py
@@ -55,14 +55,14 @@ def test_solar_day():
 
 
 def test_sunrise_mtv():
-  city = City.from_address_and_timezone('Cupertino, CA', "America/Los_Angeles")
+  city = City.get_city_from_db('Cupertino')
   panchaanga = daily.DailyPanchaanga(city=city, date=Date(year=2018, month=11, day=11))
   panchaanga.compute_sun_moon_transitions()
   numpy.testing.assert_approx_equal(panchaanga.jd_sunrise, 2458434.11)
 
 
 def test_tb_muhuurta_blr():
-  city = City.from_address_and_timezone('Bangalore', "Asia/Calcutta")
+  city = City.get_city_from_db('Bangalore')
   panchaanga = daily.DailyPanchaanga(city=city, date=Date(year=2019, month=9, day=10))
   panchaanga.compute_tb_muhuurtas()
   assert len(panchaanga.day_length_based_periods.tb_muhuurtas) == 15
@@ -71,8 +71,9 @@ def test_tb_muhuurta_blr():
     logging.info(muhurta.to_localized_string(city=city))
 
 
-def test_jd_start_orinda_ca():
-  city = City('Orinda', '37:51:38', '-122:10:59', 'America/Los_Angeles')
+def test_jd_start_DST_orinda_ca():
+  #  8:0:0.00 UT on March 9, 2019
+  city = City.get_city_from_db('Orinda')
   assert daily.DailyPanchaanga.from_city_and_julian_day(city=city,
                                                         julian_day=2458551.8333333335).julian_day_start == 2458551.8333333335
   assert daily.DailyPanchaanga.from_city_and_julian_day(city=city,
@@ -80,23 +81,33 @@ def test_jd_start_orinda_ca():
 
 
 def test_get_lagna_float():
-  city = City('X', 13.08784, 80.27847, 'Asia/Calcutta')
+  city = City.get_city_from_db('Chennai')
   numpy.testing.assert_allclose(
     city.get_lagna_float(
       2444961.7125), 10.353595502472984, rtol=1e-4)
 
 
-def test_get_anga_data():
+def test_tithi():
+  city = City.get_city_from_db('Bangalore')
+  panchaanga = daily.DailyPanchaanga(city=city, date=Date(year=2018, month=7, day=14))
+  assert panchaanga.sunrise_day_angas.tithi_at_sunrise.index == 2
+
+
+def test_get_anga_data_1981_12_23():
+  # 0:58:12.29 UT on December 23, 1981
   panchaanga = daily.DailyPanchaanga.from_city_and_julian_day(
     city=tests.spatio_temporal.chennai, julian_day=2444961.54042)
   assert panchaanga.sunrise_day_angas.tithis_with_ends[0].to_string(floating_point_precision=3) == [
     AngaSpan(anga=Anga(index=27, anga_type_id=AngaType.TITHI.name), jd_end=2444961.5992132244, jd_start=None)][0].to_string(floating_point_precision=3)
   
+  # 5:55:34.39 UT on December 23, 1981
   assert panchaanga.sunrise_day_angas.nakshatras_with_ends[0].to_string(floating_point_precision=3) == [AngaSpan(anga=Anga(index=16, anga_type_id=AngaType.NAKSHATRA.name), jd_end=2444961.746925843, jd_start=None)][0].to_string(floating_point_precision=3)
   
+  # 16:23:10.51 UT on December 23, 1981
   assert panchaanga.sunrise_day_angas.yogas_with_ends[0].to_string(floating_point_precision=3)[0] == [
     AngaSpan(anga=Anga(index=8, anga_type_id=AngaType.YOGA.name), jd_end=2444962.18276057, jd_start=None)][0].to_string(floating_point_precision=3)[0]
 
+  # 15:42:24.09 UT on December 23, 1981
   assert panchaanga.sunrise_day_angas.karanas_with_ends[0].to_string(floating_point_precision=3) == [
     AngaSpan(anga=Anga(index=54, anga_type_id=AngaType.KARANA.name), jd_end=2444961.5992132244, jd_start=None), Interval(name=55, jd_end=2444962.1544454526, jd_start=None)][0].to_string(floating_point_precision=3)
 
